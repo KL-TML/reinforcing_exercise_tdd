33dfd2044c14d5d9db2e3441874f6c2e0ee7df6e
KYLE LIPTON <lipton.kyle@gmail.com>
2017-12-11 00:20:54 -0500

    reinforcement

---
+++ calculator.rb
@@ -0,0 +1,19 @@
+def add (first, second)
+  first + second
+end
+
+def subtract(first, second)
+  first - second
+end
+
+def sum(array)
+  array.sum
+end
+
+# Set up a method below that did not sum the array
+# def sum(z)
+#   total_sum = 0
+#   z.each do |number|
+#     total_sum += number
+#   end
+# end
